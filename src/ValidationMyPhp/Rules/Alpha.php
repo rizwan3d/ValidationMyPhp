79f833aa9248dbe87ae53db47ea8d824d5a51e5d
rizwan3d3 <rizwan@ryalize.com>
2023-10-05 09:32:28 +0300

    Add: Alpha Validation

---
+++ src/ValidationMyPhp/Rules/Alpha.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rizwan3D\ValidationMyPhp\Rules;
+
+class Alpha
+{
+    /** @var string */
+    public $message = 'The %s only allows alphabet characters';
+
+    /**
+     * @param array  $data
+     * @param string $field
+     * @return bool
+     */
+    public function check(array $data, string $field): bool
+    {
+        if (!isset($data[$field])) {
+            return true;
+        }
+
+        return is_string($data[$field]) && preg_match('/^[\pL\pM]+$/u', $data[$field]);
+    }
+}
