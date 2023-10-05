385bb9eb4ed975438039ba3f0407cdd2ac6048b5
Muhammad Rizwan <rizwan3d@users.noreply.github.com>
2023-10-05 06:21:21 +0000

    Apply fixes from StyleCI

--- src/ValidationMyPhp/Rules/Url.php
+++ src/ValidationMyPhp/Rules/Url.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace Rizwan3D\ValidationMyPhp\Rules;
 
 class Url
 {
- 
     /** @var string */
     public $message = 'The %s is not valid url';
 
-   /**
+    /**
      * Return true if a string is valid URL.
      *
      * @param array  $data
@@ -19,10 +19,10 @@ class Url
     public function check(array $data, string $field, string ...$format): bool
     {
         if (count($format) == 0) {
-             return $this->validateCommonScheme($data[$field]);
+            return $this->validateCommonScheme($data[$field]);
         } else {
             foreach ($format as $scheme) {
-                $method = 'validate' . ucfirst($scheme) .'Scheme';
+                $method = 'validate'.ucfirst($scheme).'Scheme';
                 if (method_exists($this, $method)) {
                     if ($this->{$method}($data[$field])) {
                         return true;
@@ -31,14 +31,16 @@ class Url
                     return true;
                 }
             }
+
             return false;
         }
     }
 
     /**
-     * Validate $value is valid URL format
+     * Validate $value is valid URL format.
      *
      * @param mixed $value
+     *
      * @return bool
      */
     public function validateBasic($value): bool
@@ -47,10 +49,11 @@ class Url
     }
 
     /**
-     * Validate $value is correct $scheme format
+     * Validate $value is correct $scheme format.
      *
      * @param mixed $value
-     * @param null $scheme
+     * @param null  $scheme
+     *
      * @return bool
      */
     public function validateCommonScheme($value, $scheme = null): bool
@@ -63,20 +66,22 @@ class Url
     }
 
     /**
-     * Validate the $value is mailto scheme format
+     * Validate the $value is mailto scheme format.
      *
      * @param mixed $value
+     *
      * @return bool
      */
     public function validateMailtoScheme($value): bool
     {
-        return $this->validateBasic($value) && preg_match("/^mailto:/", $value);
+        return $this->validateBasic($value) && preg_match('/^mailto:/', $value);
     }
 
     /**
-     * Validate the $value is jdbc scheme format
+     * Validate the $value is jdbc scheme format.
      *
      * @param mixed $value
+     *
      * @return bool
      */
     public function validateJdbcScheme($value): bool
